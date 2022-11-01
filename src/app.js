53eff8872cc724065a274227c8ac3fef59c13009
mjoy1113 <mjoy1113@gmail.com>
2022-11-01 17:37:47 -0400

    Set to show current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#speed");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "7a1786b0693c1b77d9103a1ed9f5371b";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=charlotte&appid=${apiKey}&units=imperial`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
