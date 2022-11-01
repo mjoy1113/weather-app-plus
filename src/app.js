56010b85d245e8f23722e4d4c69d68888edc8cc0
mjoy1113 <mjoy1113@gmail.com>
2022-11-01 17:55:38 -0400

    Removed console.logs

--- src/app.js
+++ src/app.js
@@ -22,7 +22,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -40,5 +39,4 @@ function displayTemperature(response) {
 let apiKey = "7a1786b0693c1b77d9103a1ed9f5371b";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=charlotte&appid=${apiKey}&units=imperial`;
 
-console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
